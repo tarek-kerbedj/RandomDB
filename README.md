95c86308892cccdb2c563064ae04966084dd5bf1
Oxidiovega <tarekvega92@gmail.com>
2022-05-07 15:22:43 +0200

    first demo

---
+++ README.md
@@ -0,0 +1,32 @@
+### About Dataset
+The movie dataset includes 85,855 movies with attributes such as movie description, average rating, votes, Year, Date published, Title, description, genre, etc.
+
+### Form Big Idea
+•	Demonstrate the true understanding of what the data says using Visualisation.
+The data sets contain the information about IMDB movies website which is most popular at that moment for online movie streaming. It combines movie plot description. Ratings, critic and user ratings, release dates and many more aspects. This data can be plotted in different categories which includes bar graph, scatter plot, cloud representation, etc.
+
+For example, if we plot the genre column and find the Top 3 Movie Genres, then we can plot it something like this:-
+
+![top3genres](https://user-images.githubusercontent.com/47186806/117329468-91d14e80-ae8c-11eb-8ab2-25a9ca9aac39.PNG)
+
+![cloud](https://user-images.githubusercontent.com/47186806/117329475-9269e500-ae8c-11eb-97f5-c14b8649e403.PNG)
+ 
+ 
+As we can see from the above 2 graphs( bar chart and wordcloud) – Drama, comedy and action are the top 3 movie genres which are watched by people all around the world on IMDB website.
+
+Now we can see top 25 directors of all times who created the movies.
+
+![director](https://user-images.githubusercontent.com/47186806/117329477-93027b80-ae8c-11eb-883c-f4081d94ac63.PNG)
+ 
+
+Lastly, there is a huge difference between the languages spoken on movies released.
+
+![language](https://user-images.githubusercontent.com/47186806/117329457-90a02180-ae8c-11eb-8fc7-175f03e6002d.PNG)
+ 
+As we can see the most widely used language across all kinds of movies is English or in simple most movies are made in English and it’s because it’s the only language which is spoken around all the world.
+
+Below is the scatter plot for displaying highest number of the movies generated in specific countries.
+
+![scatterplot](https://user-images.githubusercontent.com/47186806/117329462-9138b800-ae8c-11eb-992e-d129b57f3934.PNG)
+ 
+As we can see from the scatter plot up there, USA and UK have the highest number of movies produced and also, they have both low and high user reviews of movies.
---
+++ movies.py
@@ -0,0 +1,126 @@
+import pandas as pd
+import streamlit as st
+import math
+import random
+import numpy as np
+from time import perf_counter
+from urllib3 import PoolManager
+from PIL import Image
+from io import BytesIO
+from bs4 import BeautifulSoup
+import os
+st.set_page_config(page_title='RandomDB',layout='wide', page_icon="🎥")
+https = PoolManager()
+
+st.markdown("""
+<style>
+div.stButton > button:first-child {
+    background-color: #000000;
+    color:#ffffff;
+}
+div.stButton > button:hover {
+    background-color: #FF0000;
+    color:##ff99ff;
+    }
+</style>""", unsafe_allow_html=True)
+st.markdown("""
+        <style>
+               .css-18e3th9 {
+                    padding-top: 0rem;
+                    padding-bottom: 10rem;
+                    padding-left: 5rem;
+                    padding-right: 5rem;
+                }
+               .css-1d391kg {
+                    padding-top: 3.5rem;
+                    padding-right: 1rem;
+                    padding-bottom: 3.5rem;
+                    padding-left: 1rem;
+                }
+        </style>
+        """, unsafe_allow_html=True)
+base_url='https://www.movieposterdb.com/'
+
+def get_data():
+    return pd.read_csv('movies.csv',dtype={"year": "int16","duration":"int16","votes":"int32",'avg_vote':'float32'})
+movies=get_data()
+st.markdown(""" <style text-align=center> .font {
+font-size:30px ; font-family: 'Brush Script MT', cursive; color: #ffffff;} 
+</style> """, unsafe_allow_html=True)
+#<div style="text-align: center"> your-text-here </div>
+#st.markdown('<p class="font" ,>IMDB Movies</p>', unsafe_allow_html=True)
+placeholder = st.empty()
+placeholder.markdown("<h1 style='text-align: center; color: white;'>&#127902;&#65039 IMDB randomizer &#127902;&#65039 </h1>", unsafe_allow_html=True)
+#st.markdown("""<p style="font-size:48px">
+#&#128512; &#128516; &#128525;&#127909;
+#</p>""",unsafe_allow_html=True)
+genre=st.sidebar.selectbox("Genre", ("unspecified","Drama","Action", "Documentary", "Comedy","Adventure","Crime","Romance","Fantasy","Animation"
+,"Mystery","Biography"))
+language=st.sidebar.selectbox('Language', ('unspecified','Arabic','English','French','German',"Hindi","Italian",'Japanese','Korean','Spanish',"Turkish")) 
+#director=st.sidebar.selectbox('Director',["john","jake","ahmed"]) 
+rating=st.sidebar.slider("minimal rating", min_value=1, step=1,max_value=10)
+year=st.sidebar.slider('movie release year',min_value=1900,max_value=2019,value=(1950,2019))
+#actor=st.selectbox('Example',["john","jake","ahmed"])
+
+
+#with st.sidebar.expander("Advanced options"):
+
+
+col1, col2,col3= st.sidebar.columns([1,1,1])
+col5,col6,col7=st.columns([1,3,1])
+random_pick=col2.button('Search')
+
+
+if year:
+    begin,end=year
+
+if random_pick:
+    begin,end=year
+    expr=movies[(movies['year']>begin) & (movies['year']<end) &(movies['avg_vote']>rating)]
+    if language=='unspecified' and genre=='unspecified':
+        ls1=expr
+    elif language=='unspecified' and genre !='unspecified':
+        ls1=expr[expr['genre'].str.match(genre)]
+    elif language !='unspecified' and genre =='unspecified':
+        ls1=expr[expr['language']==language]
+    else:
+         ls1=movies[(movies['year']>begin) & (movies['year']<end) &(movies['avg_vote']>rating) &(movies['language']==language)&(movies['genre'].str.match(genre)==True)]
+   # st.subheader(ls1.shape[0])
+    pick=random.randint(0,ls1.shape[0])
+        #pick=random.randint(0,ls1.shape[0])
+    
+    try:
+        
+        placeholder.empty()
+        imdb_id=base_url+ls1.iloc[pick]['imdb_title_id']       
+        response = https.request('GET',imdb_id)
+        soup = BeautifulSoup(response.data, 'html')
+
+        i=soup.select_one("body > div.section > div.container > div.row.mt-5.mb-4 > div.col-md-4.ml-auto > figure > a > img")['src']
+        col6.header(ls1.iloc[pick]['original_title']+' '+'('+str(ls1.iloc[pick]['year'])+')')
+        col6.subheader("rated "+str(ls1.iloc[pick]['avg_vote'])+"/ 10")
+        col6.image(i)
+        with st.expander("movie description"):
+ 
+                st.write(ls1.iloc[pick]['description'])
+
+    except(TypeError):
+        #col6.image('https://c.tenor.com/yTEHRx1ofG4AAAAC/umaru-chan-tears.gif')
+        
+        col6.header(ls1.iloc[pick]['original_title']+' '+'('+str(ls1.iloc[pick]['year'])+')')
+        col6.subheader("rated "+str(ls1.iloc[pick]['avg_vote'])+"/ 10")
+        col6.image('https://c.tenor.com/YM3fW1y6f8MAAAAC/crying-cute.gif',caption='no poster found')
+
+    except(IndexError):
+        col5,col6,col7=st.columns([1,2,1])
+        #col6.subheader('try lowering your expectations')
+        #st.image('https://c.tenor.com/yTEHRx1ofG4AAAAC/umaru-chan-tears.gif')
+        col6.image('megamind.png',caption="Try (unironically) lowering your expectations.")
+        #st.markdown("""<img src="megamind.png" style="width:100%;">""",unsafe_allow_html=True)
+    #show title
+   
+    #show votes
+   
+
+    #show poster
+    
